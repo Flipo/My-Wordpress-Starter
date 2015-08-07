d6dbeaed46ed8ac04ca21be75a88962b5e6d32e3
Philipp Seiffert <ph.seiffert@gmx.de>
2015-08-07 13:18:12 +0200

    Thumbnail Sizes

--- functions.php
+++ functions.php
@@ -27,11 +27,11 @@ if (function_exists('add_theme_support'))
 
     // Add Thumbnail Theme Support
     add_theme_support('post-thumbnails');
-    add_image_size('large', 700, '', true); // Large Thumbnail
-    add_image_size('medium', 250, '', true); // Medium Thumbnail
-    add_image_size('small', 120, '', true); // Small Thumbnail
+    add_image_size('xlarge', 1500, '', true); // Large Thumbnail
+    add_image_size('large', 1000, '', true); // Large Thumbnail
+    add_image_size('medium', 500, '', true); // Medium Thumbnail
+    add_image_size('small', 250, '', true); // Small Thumbnail
     add_image_size('facebook', 1800, 1125, true); // Facebook Thumbnail
-    add_image_size('custom-size', 700, 200, true); // Custom Thumbnail Size call using the_post_thumbnail('custom-size');
 
     // Add Support for Custom Backgrounds - Uncomment below if you're going to use
     /*add_theme_support('custom-background', array(
